7bf05b27a0cf1ea9d2be5366c011ee2a1ad1221e
Trent Rosane <trosane@uw.edu>
2015-11-30 20:46:24 -0800

    fixed matrix

--- Assignment2.R
+++ Assignment2.R
@@ -22,37 +22,37 @@ rawMatrix <- mat.or.vec(6, 6)
 rawMatrix[1,1] = 1
 rawMatrix[1,2] = 0
 rawMatrix[1,3] = 2
-rawMatrix[1,2] = 0
+rawMatrix[1,4] = 0
 rawMatrix[1,5] = 4
 rawMatrix[1,6] = 3
 rawMatrix[2,1] = 3
 rawMatrix[2,2] = 0
 rawMatrix[2,3] = 1
-rawMatrix[2,2] = 1
+rawMatrix[2,4] = 1
 rawMatrix[2,5] = 0
 rawMatrix[2,6] = 0
 rawMatrix[3,1] = 2
 rawMatrix[3,2] = 0
 rawMatrix[3,3] = 4
-rawMatrix[3,2] = 0
+rawMatrix[3,4] = 0
 rawMatrix[3,5] = 1
 rawMatrix[3,6] = 0
 rawMatrix[4,1] = 0
 rawMatrix[4,2] = 0
 rawMatrix[4,3] = 1
-rawMatrix[4,2] = 0
+rawMatrix[4,4] = 0
 rawMatrix[4,5] = 0
 rawMatrix[4,6] = 1
 rawMatrix[5,1] = 8
 rawMatrix[5,2] = 0
 rawMatrix[5,3] = 3
-rawMatrix[5,2] = 0
+rawMatrix[5,4] = 0
 rawMatrix[5,5] = 5
 rawMatrix[5,6] = 2
 rawMatrix[6,1] = 0
 rawMatrix[6,2] = 0
 rawMatrix[6,3] = 0
-rawMatrix[6,2] = 0
+rawMatrix[6,4] = 0
 rawMatrix[6,5] = 0
 rawMatrix[6,6] = 0
 
@@ -77,13 +77,9 @@ for (i in 1:rowCount) {
   for (k in 1:rowCount) {
     if (diagonalAdjustMatrix[i,k] != 0) {
       diagonalAdjustMatrix[i,k] <- (diagonalAdjustMatrix[i,k]/sums[k])
-      flag <- FALSE
     } else {
-      flag <- TRUE
+
     }
-  }
-  if (flag <- FALSE) {
-    
   }
 }
 
